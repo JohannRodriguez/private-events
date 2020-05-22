2eddb887e60a56bbb4a8671af56b8529e14a58f1
carlosprietobarron <carlosprietobarron@gmail.com>
2020-05-22 13:32:44 -0700

    Creat list of User events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_171913) do
+ActiveRecord::Schema.define(version: 2020_05_22_201250) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_05_22_171913) do
     t.text "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
