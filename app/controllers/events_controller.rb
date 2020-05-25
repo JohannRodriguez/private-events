7e2806a76900ce5259ae1c03a1cf48e878087d47
Johann <themrblacksapce@hotmail.com>
2020-05-25 14:23:35 -0600

    List all created events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,8 @@ class EventsController < ApplicationController
     @event = Event.new
     @event.user_id = session[:user_id]
     @event.name = params[:name]
+    @event.event_date = params[:event_date]
+    @event.location = params[:location]
     if @event.save
       redirect_to users_show_path, notice:'Your event was created'
     else
@@ -18,5 +20,6 @@ class EventsController < ApplicationController
   end
 
   def index
+    @event = Event.find_each
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,3 @@
 class Event < ApplicationRecord
-    belongs_to :User
+    belongs_to :user
 end
