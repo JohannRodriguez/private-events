910fc7998dcf3e9fc91c1c0a268569b2bf140438
carlosprietobarron <carlosprietobarron@gmail.com>
2020-05-26 14:50:48 -0700

    Create list of upcoming and previous events for user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,19 @@ module UsersHelper
   def full_event_name(event)
     "#{event.name} at #{event.event_date} in  #{event.location}"
   end
+
+  def upcoming_events(user)
+    list=[]
+    user.invitations.each do |i|
+      list.push i if i.attended_event.event_date >Time.now
+    end
+    return list
+  end
+  def previous_events(user)
+    list=[]
+    user.invitations.each do |i|
+      list.push i if i.attended_event.event_date <= Time.now
+    end
+    return list
+  end
 end
