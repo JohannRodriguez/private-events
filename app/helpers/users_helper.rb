b3274255cb2542f669b712f98b1e995f2297e7c2
Johann <themrblacksapce@hotmail.com>
2020-05-26 15:00:39 -0600

    Previous and past events add to show view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,8 +8,4 @@ module UsersHelper
   def full_event_name(event)
     "#{event.name} at #{event.event_date} in  #{event.location}"
   end
-
-  def attended_event(item)
-    @event = item.attended_event.name
-  end
 end
