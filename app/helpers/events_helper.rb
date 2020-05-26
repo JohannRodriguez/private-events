3c15489d7cf507b938171a33942a4323dc0193a5
carlosprietobarron <carlosprietobarron@gmail.com>
2020-05-26 10:40:15 -0700

    Create event show page with attendees list

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -8,4 +8,8 @@ module EventsHelper
   def full_event_name(event)
     "#{event.name} at #{event.event_date} in  #{event.location}"
   end
+  def attendace(event)
+    return "Attendees List" unless event.invitations.count==0
+    "No Attendees yet"  
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,4 @@ class User < ApplicationRecord
   has_many :events
   has_many :invitations, foreign_key: :attendee_id
   has_many :attended_events, through: :invitations
-  
 end
