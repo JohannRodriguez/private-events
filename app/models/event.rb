fe14336245d1c294c6078a390267528803f7527b
carlosprietobarron <carlosprietobarron@gmail.com>
2020-05-27 08:44:08 -0700

    Refactor Upcoming and Previous Events Methods to Scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,19 +3,10 @@ class Event < ApplicationRecord
 
   has_many :invitations, foreign_key: :attended_event_id
   has_many :attendees, through: :invitations
-  def self.upcoming
-    list=[]
-    Event.all.each do |i|
-      list.push i if i.event_date > Time.now
-    end
-    return list
-  end
-  
-  def self.previous
-    list=[]
-    Event.all.each do |i|
-      list.push i if i.event_date <= Time.now
-    end
-    return list
-  end
+
+  scope :upcoming, -> { where('event_date > ?', Time.now.to_date) }
+ 
+  scope :previous, -> { where('event_date < ?', Time.now.to_date) }
+
+ 
 end
