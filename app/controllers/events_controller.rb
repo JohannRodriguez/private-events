47200ae6f259a72c032834f726428f7aedb6b517
carlosprietobarron <carlosprietobarron@gmail.com>
2020-06-02 12:11:39 -0700

    Use strong parameters in eventnew view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,7 @@ class EventsController < ApplicationController
 
   def create
     @user = User.find(session[:user_id])
-    @event = @user.created_events.build(event_params)
+    @event = @user.created_events.build(event_params(params[:name], params[:event_date], params[:location]))
     if @event.valid?
       if @event.save
         redirect_to users_show_path, notice: 'Your event was created'
@@ -27,8 +27,9 @@ class EventsController < ApplicationController
     @event_previous = Event.previous
   end
 
-  def event_params
-    params.permit(:created_event).permit(:name, :event_date, :location)
+  def event_params(_name, _event_date, _location)
+    params = ActionController::Parameters.new(event: { name: _name, event_date: _event_date, location: _location })
+    params.require(:event).permit(:name, :event_date, :location)
   end
   # rubocop:enable Layout/LineLength
 end
